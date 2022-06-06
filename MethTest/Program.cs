6aa0bea6b537e5123dc0afd1f1cc967f4c62b4a9
Michael <Michael@LAPTOP-M3UI0TO9>
2022-06-05 22:09:33 -0500

    Add project files.

---
+++ MethTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class MethTest
+{
+    static void MathMeth( int x,int y, out int sum)
+    {
+        sum = x + y;
+    }
+    static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int sum;
+        MathMeth(a, b, out sum);
+        int sum1 = sum;
+        
+        int c = Convert.ToInt32(Console.ReadLine());
+        int d = Convert.ToInt32(Console.ReadLine());
+        MathMeth(c, d, out sum);
+        int sum2 = sum;
+        Console.WriteLine(sum1 + ", " + sum2);
+        Console.ReadKey();
+    }
+}
