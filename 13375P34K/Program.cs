d5d29a33582f075fb631708bc5b3c8437a43daba
MichaelPWeston <106941556+MichaelPWeston@users.noreply.github.com>
2022-06-21 16:59:52 -0500

    Add files via upload

---
+++ 13375P34K/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+public class Program
+{
+	public static string HackerSpeak(string str)
+	{
+		var characters = new Dictionary<string, string>()
+		{
+			{ "a", "4" },
+			{ "e", "3" },
+			{ "i", "1" },
+			{ "o", "0" },
+			{ "s", "5" },
+			{ "t", "7" }
+		};
+		foreach (var chars in characters)
+        {
+			str = str.Replace(chars.Key, chars.Value );
+        }
+		return str;
+		
+	}
+
+	static void Main()
+    {
+		string userString = Console.ReadLine();
+		Console.WriteLine(HackerSpeak(userString));
+    }
+}
---
+++ fizzbuzz/fizzbuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class FizzBuzz
+{
+    static void Main()
+    {
+        int x, y;
+        Console.WriteLine("Please enter first value:");
+
+        
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter second value");
+        y = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("\n");
+
+        for (int i = 1; i <= 100; i++)
+        {
+            string output = "";
+
+            if (i % x == 0) { output += "fizz";}
+            if (i % y == 0) { output += "buzz";}
+            if (output == "") { output = i.ToString();}
+            Console.WriteLine(output);
+        }
+        
+        
+
+    }
+}
